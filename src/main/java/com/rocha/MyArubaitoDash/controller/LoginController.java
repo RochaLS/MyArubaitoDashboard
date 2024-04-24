665aa81c43ea28ab698033b1d00f56d96732dca3
RochaLS <ld.rocha@hotmail.com>
2024-04-24 12:18:41 -0700

    remove unecessary session class, as we are using in memory sessions

--- src/main/java/com/rocha/MyArubaitoDash/controller/LoginController.java
+++ src/main/java/com/rocha/MyArubaitoDash/controller/LoginController.java
@@ -1,27 +1,14 @@
 package com.rocha.MyArubaitoDash.controller;
 
 
-import com.rocha.MyArubaitoDash.model.Session;
-import com.rocha.MyArubaitoDash.model.Worker;
 import com.rocha.MyArubaitoDash.security.SecurityUser;
-import com.rocha.MyArubaitoDash.service.SessionService;
 import jakarta.servlet.http.HttpSession;
-import org.joda.time.DateTime;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
 @RestController
 public class LoginController {
-
-    private final SessionService sessionService;
-
-    public LoginController(SessionService sessionService) {
-        this.sessionService = sessionService;
-    }
 
 
 
