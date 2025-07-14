9831a052571e33d1a42d536b4b6055cb4c3e3801
Lucas Rocha <ld.rocha@hotmail.com>
2025-07-13 23:39:08 -0700

    add formatter

--- src/main/java/com/rocha/MyArubaitoDash/util/SafeLocalTimeDeserializer.java
+++ src/main/java/com/rocha/MyArubaitoDash/util/SafeLocalTimeDeserializer.java
@@ -18,6 +18,7 @@ public class SafeLocalTimeDeserializer extends JsonDeserializer<LocalTime> {
     private static final Logger logger = LoggerFactory.getLogger(SafeLocalTimeDeserializer.class);
     private static final DateTimeFormatter[] formatters = new DateTimeFormatter[] {
             DateTimeFormatter.ofPattern("HH:mm:ss"),
+            DateTimeFormatter.ofPattern("H:mm:ss"),
             DateTimeFormatter.ofPattern("HHmm"),
             DateTimeFormatter.ofPattern("HH:mm"),
             DateTimeFormatter.ofPattern("Hmm"),
